02f0a97e3c2c8dbd80feef4a8de132c3ccf445e3
lukinhag12 <lukinha.g12.g12@gmail.com>
2021-10-25 22:50:08 -0300

    add eslint - tailwind - router - store - vee-validate - axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import router from '@/router';
+import store from '@/store';
+import App from '@/App.vue';
+import '@/plugins/vee-validate';
+import '@/plugins/axios';
 
 import '@/assets/css/tailwind.css';
 
@@ -10,5 +12,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
