05269422baef042e60b9f056763adfaea8a0dd02
lukinhag12 <lukinha.g12.g12@gmail.com>
2021-10-27 13:23:23 -0300

    ajuste no axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,61 +1,22 @@
-'use strict';
 
-import Vue from 'vue';
+
 import axios from 'axios';
+import Vue from 'vue';
+import Cookie from '@/service/cookie';
 
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = 'http://api.todolist.test/api';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Accept'] = 'application/json';
 
-let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
-};
+axios.interceptors.request.use(function(config) {
+    const token = Cookie.getToken();
 
-const _axios = axios.create(config);
+    if (token) {
+        config.headers.common['Authorization'] = token;
+    }
 
-_axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
     return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  },
-);
+});
 
-// Add a response interceptor
-_axios.interceptors.response.use(
-  function(response) {
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  },
-);
+Vue.prototype.$axios = axios;
 
-Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      },
-    },
-    $axios: {
-      get() {
-        return _axios;
-      },
-    },
-  });
-};
-
-Vue.use(Plugin);
-
-export default Plugin;
